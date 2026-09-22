abf46b90a98a23e3266a6b3594df86b4f2512edc
cielo <cielo@CIELO>
2026-09-21 22:33:23 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EJERCICIOS_DE_ORDENAMIENTO
+{
+    internal class Program
+    {
+        static public void Leer(int[] N)
+        {
+            for (int i = 0; i < N.Length; i++)
+            {
+                Console.Write("edad[" + i + "]=");
+                N[i] = int.Parse(Console.ReadLine());
+            }
+        }
+        static public void Escribir(int[] N)
+        {
+            for (int i = 0; i < N.Length; i++)
+            {
+                Console.WriteLine("edad[" + i + "]=" + N[i]);
+            }
+        }
+
+        static public void ordenar_Asc(int[] N)
+        {
+            int aux;
+            for (int i = 0; i < N.Length; i++)
+            {
+                for (int j = 0; j < N.Length - 1 - i; j++)
+                {
+                    if (N[j] > N[j + 1])
+                    {
+                        aux = N[j];
+                        N[j] = N[j + 1];
+                        N[j + 1] = aux;
+                    }
+
+                }
+            }
+        }
+
+        static public void ordenar_desc(int[] N)
+        {
+            int aux;
+            for (int i = 0; i < N.Length; i++)
+            {
+                for (int j = 0; j < N.Length - 1 - i; j++)
+                {
+                    if (N[j] < N[j + 1])
+                    {
+                        aux = N[j];
+                        N[j] = N[j + 1];
+                        N[j + 1] = aux;
+                    }
+
+                }
+            }
+        }
+        static public void orden_seleccion(int[] N)
+        {
+            int aux;
+            for (int i = 0; i < N.Length - 1; i++)
+            {
+                int minimo = i;
+                for (int j = i + 1; j < N.Length; j++)
+                {
+                    if (N[j] < N[minimo])
+                    {
+                        minimo = j;
+
+                    }
+                }
+                aux = N[i];
+                N[i] = N[minimo];
+                N[minimo] = aux;
+            }
+        }
+        
+        static public void orden_seleccion_des(int[] N)
+        { 
+            int aux;
+            for (int i = 0; i<N.Length - 1; i++)
+            {
+                int minimo = i;
+                for (int j = i + 1; j<N.Length; j++)
+                {
+                    if (N[j] > N[minimo])
+                    {
+                        minimo = j;
+
+                    }
+
+                     aux = N[i];
+                    N[i] = N[minimo];
+                    N[minimo] = aux;
+                    }
+            }
+        }
+            static void Main(string[] args)
+                {
+                    int n;
+                    Console.Write("ingrese dimensión del arreglo:");
+                    n = int.Parse(Console.ReadLine());
+                    int[] edad = new int[n];
+                    Leer(edad);
+                    Console.WriteLine("****************************");
+                    Console.WriteLine("ARREGLO SIN ORDENAR");
+                    Console.WriteLine("****************************");
+                    Escribir(edad);
+                    Console.WriteLine("****************************");
+                    Console.WriteLine("ORDENAR ASCENDENTE");
+                    Console.WriteLine("****************************");
+                    ordenar_Asc(edad);
+                    Escribir(edad);
+                    Console.WriteLine("****************************");
+                    Console.WriteLine("ORDENAR DESCENDENTE");
+                    Console.WriteLine("****************************");
+                    ordenar_desc(edad);
+                    Escribir(edad);
+                     Console.WriteLine("****************************");
+                     Console.WriteLine("ORDENAMIENTO ASCENDENTE POR SELECCION");
+                    Console.WriteLine("**********************************");
+                    orden_seleccion(edad);
+                    Escribir (edad);
+                    Console.WriteLine("****************************");
+                    Console.WriteLine("ORDENAMIENTO DESCENDENTE POR SELECCION");
+                    Console.WriteLine("**********************************");
+                    orden_seleccion_des(edad);
+                    Escribir(edad);
+
+            Console.ReadKey();
+
+                }
+            
+
+        
+
+    }
+}
+            
+        
+    
+
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("EJERCICIOS DE ORDENAMIENTO")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EJERCICIOS DE ORDENAMIENTO")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("8f323b0b-7f22-49dd-91cf-a626b3cfc3b7")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
